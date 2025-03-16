10536df4932bbda1a22e746261c2bd1331399721
Onkar Dhengale <onkardhengale@gmail.com>
2025-03-16 21:10:01 +0530

    organize and tree implemented

--- organized_files/documents/main.js
+++ organized_files/documents/main.js
@@ -18,7 +18,6 @@ let command= inputArray[0];
 //console.log(command);
 switch(command){
     case "tree":
-        treeFn(inputArray[1]);
         break;
     case "organize":
         organizeFn(inputArray[1]);
@@ -32,42 +31,6 @@ switch(command){
 }
 function treeFn(dirpath){
     console.log("Tree command will be executed...")
-    let destinationPath;
-    // console.log("organize command will be executed...");
-    // 1)take input directory 
-    // 2)create folder organized_files
-    // 3)identify categories of all files present in dirpath
-    // 4)copy/cut any file to right destination folder.
-
-    // 1)take input path given
-    if(dirpath==undefined){
-        console.log("Kindly enter the path");
-        return;
-    }else{
-        if(fs.existsSync(dirpath)){
-            treeHelper(dirpath,"");
-        }else{
-            console.log("kindly enter the correct path")
-        }
-        
-    }
-
-}
-function treeHelper(dirpath, indent){
-    //isFile or folder
-    let isFile= fs.lstatSync(dirpath).isFile();
-    if(isFile){
-        let filename= path.basename(dirpath)
-        console.log(indent, "├──",filename);
-    }else{
-        let dirname= path.basename(dirpath);
-        console.log(indent,"└──",dirname);
-        let children=fs.readdirSync(dirpath);
-        for(let i=0; i<children.length; i++){
-            let childpath= path.join(dirpath,children[i]);
-            treeHelper(childpath,indent+"\t");
-        }
-    }
 }
 
 function organizeFn(dirpath){
@@ -89,8 +52,6 @@ function organizeFn(dirpath){
                 fs.mkdirSync(destinationPath);
             }
             organizeHelper(dirpath, destinationPath)
-        }else{
-            console.log("Kindly enter the correct path")
         }
         
     }
@@ -122,7 +83,6 @@ function sendFiles(srcFilePath,dest,category){
     let fileName= path.basename(srcFilePath);
     let destFilePath= path.join(categoryPath,fileName);
     fs.copyFileSync(srcFilePath,destFilePath);
-    // fs.unlinkSync(srcFilePath);//to cut the file
 
 }
 function getCategory(name){
---
+++ organized_files/others/README.md
@@ -0,0 +1,6 @@
+# FileSystemAnalyser
+
+Functionalities:
+    1)make tree: This will give you tree structure of any folder.
+    2)make organize: This will organize a folder based on formats like audio, video, etc.
+    3)make help: This will list down all the commands available to use.(Helper).
